a9e1adc904cc6d87e48c0ae77acadfb68755f039
jsar3004 <jsar3004@gmail.com>
2022-11-06 18:18:15 +0530

    removed image

--- frontend/src/Components/Main/Main.js
+++ frontend/src/Components/Main/Main.js
@@ -26,18 +26,7 @@ function Main(){
     </li>
  </ul>
  </div>
- <div className="basedesc">
- <div className="seconddesc">
-    <h1 className="heading1">Create Certificates Online or Auto Generate via API</h1>
-    <h3 className="heading2">Easily create certificates and awards online to recognize the achievements of your employees, speakers, students etc.</h3>
-    <h3 className="heading2">Simply enter some details and generate your certificate PDF in one click. You can download up to 5 beautiful PDF certificates per day for free.</h3>
-    <h3 className="heading2">Creating a lot of certificates and need to automate the process? Look into our API or Nocode automations to generate PDF certificates in bulk.</h3>
-   
- </div>
-  <div className="secondimage seconddesc">
-   <img className="image" src="https://d33wubrfki0l68.cloudfront.net/646261c1acc41707b6253e4c0aca0c5d0aceb64a/7c076/images/oslo_illustrations/15_power_transp.png"></img>
- </div>
- </div>
+
 </>
     );
 }
